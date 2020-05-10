a00accd72b287d9e5e6113f781c686c352c58040
Kristof <kristof.martens@gmail.com>
2020-05-10 22:14:43 +0200

    Some code reorganisation

---
+++ mproxy/config.go
@@ -0,0 +1,59 @@
+package mproxy
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+)
+
+type OIDCClaim struct {
+	ClaimName     string
+	AllowedClaims []string
+}
+
+type ProxyRule struct {
+	Pattern string
+	Claims  []OIDCClaim
+}
+
+type Config struct {
+	LocalPort   uint16
+	Destination string
+	ProxyRules []ProxyRule
+}
+
+func IsValidConfig(config Config) bool {
+	switch {
+	case len(config.Destination) == 0:
+		return false
+	}
+
+	return true
+}
+
+
+func GetDefaultConfig() Config {
+	config := Config{
+		LocalPort:   8080,
+		Destination: "",
+		ProxyRules: []ProxyRule{{
+			Pattern: "/",
+			Claims:  nil,
+		}},
+	}
+
+	return config
+}
+
+func (c Config) getListenAddress() string {
+	return fmt.Sprintf(":%d", c.LocalPort)
+}
+
+func (c Config) getURL() *url.URL {
+	destURL, ok := url.Parse(c.Destination)
+	if ok != nil {
+		log.Fatal("Invalid destination", ok)
+	}
+	return destURL
+}
+
--- mproxy/mproxy.go
+++ mproxy/mproxy.go
@@ -2,27 +2,11 @@ package mproxy
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
-type OIDCClaim struct {
-	ClaimName     string
-	AllowedClaims []string
-}
-
-type ProxyRule struct {
-	Pattern string
-	Claims  []OIDCClaim
-}
-
-type Config struct {
-	LocalPort   uint16
-	Destination string
-	ProxyRules []ProxyRule
-}
 
 type MProxy struct {
 	config        Config
@@ -36,19 +20,6 @@ const (
 	ErrorInvalidConfig
 	ErrorProxyInitError
 )
-
-func GetDefaultConfig() Config {
-	config := Config{
-		LocalPort:   8080,
-		Destination: "",
-		ProxyRules: []ProxyRule{{
-			Pattern: "/",
-			Claims:  nil,
-		}},
-	}
-
-	return config
-}
 
 func CreateProxy(config Config) (MProxy, int) {
 	ok := ErrorNoError
@@ -70,13 +41,16 @@ func CreateProxy(config Config) (MProxy, int) {
 func (p *MProxy) StartProxy() int {
 	ok := ErrorNoError
 
-	// Configure the different paths to proxy and their authorization rules
-	http.HandleFunc("/",
-		func(writer http.ResponseWriter, request *http.Request) {
-			fmt.Println("headers:", request.Header)
-			fmt.Println("Body:", request.Body)
-			httputil.NewSingleHostReverseProxy(p.destURL).ServeHTTP(writer, request)
-		})
+	for _, proxyRule := range p.config.ProxyRules {
+		// Configure the different paths to proxy and their authorization rules
+		http.HandleFunc(proxyRule.Pattern,
+			func(writer http.ResponseWriter, request *http.Request) {
+				fmt.Println("claims config:", proxyRule.Claims)
+				fmt.Println("headers:", request.Header)
+				fmt.Println("Body:", request.Body)
+				httputil.NewSingleHostReverseProxy(p.destURL).ServeHTTP(writer, request)
+			})
+	}
 
 	// Set running state to true
 	p.setRunning(true)
@@ -93,25 +67,4 @@ func (p *MProxy) StartProxy() int {
 
 func (p *MProxy) setRunning(running bool) {
 	p.running = running
-}
-
-func IsValidConfig(config Config) bool {
-	switch {
-	case len(config.Destination) == 0:
-		return false
-	}
-
-	return true
-}
-
-func (c Config) getListenAddress() string {
-	return fmt.Sprintf(":%d", c.LocalPort)
-}
-
-func (c Config) getURL() *url.URL {
-	destURL, ok := url.Parse(c.Destination)
-	if ok != nil {
-		log.Fatal("Invalid destination", ok)
-	}
-	return destURL
 }
